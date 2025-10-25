3183c34163d02e845eafa8b987d3d0fc23b3c898
MeroFuruya <80584154+MeroFuruya@users.noreply.github.com>
2025-10-25 21:47:12 +0200

    Set initial password for user 'mero' in configuration.nix

--- systems/remote/hetzner-nix1/configuration.nix
+++ systems/remote/hetzner-nix1/configuration.nix
@@ -44,6 +44,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mero = {
+    initialPassword = "mero";
     isNormalUser = true;
     extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
